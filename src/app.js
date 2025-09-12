483f232645903b2a68674cc2de20b0e5b4e5bef3
habul17 <abulhassanca.dev@gmail.com>
2025-09-12 09:07:06 +0530

    added api level validation

--- src/app.js
+++ src/app.js
@@ -67,10 +67,23 @@ app.delete("/user", async (req, res) => {
 
 // update the user
 
-app.patch("/user", async (req, res) => {
-    const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params?.userId;
     const data = req.body;
+
     try {
+        const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+        const isUpdateAllowed = Object.keys(data).every((k) =>
+            ALLOWED_UPDATES.includes(k)
+        );
+
+        if (!isUpdateAllowed) {
+            throw new Error("Update Not Allowed")
+        }
+
+        if (data.skills && Array.isArray(data.skills) && data.skills.length > 5) {
+            throw new Error("Skill cant be more tha 5")
+        }
         const user = await User.findByIdAndUpdate(userId, data, { returnDocument: "after", runValidators: true });
         console.log(user);
         res.send("User updated successfully")
